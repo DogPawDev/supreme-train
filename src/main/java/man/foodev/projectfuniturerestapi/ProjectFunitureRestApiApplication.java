9e447d922ca42626147c920c80f75eeb5010f169
foodev <foodev@naver.com>
2020-12-28 19:17:06 +0900

    project init

---
+++ src/main/java/man/foodev/projectfuniturerestapi/ProjectFunitureRestApiApplication.java
@@ -0,0 +1,13 @@
+package man.foodev.projectfuniturerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFunitureRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFunitureRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/man/foodev/projectfuniturerestapi/ProjectFunitureRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package man.foodev.projectfuniturerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFunitureRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
